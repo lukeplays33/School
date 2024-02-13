14a54ab687b7dc2d9072a668a6c3fe770e4b46c3
= <lukehoogenboom@gmail.com>
2024-02-13 11:58:16 +0100

    test

--- pages/responsive/js/main.js
+++ pages/responsive/js/main.js
@@ -2,6 +2,7 @@ let nav = document.getElementsByTagName('nav')[0];
 
 document.getElementById('cheeseBurger').onclick = function () {
     if(nav.style.dispaly == 'flex') {
+        alert('test')
         nav.style.dispaly = 'none';
     } else {
         nav.style.dispaly = 'flex';
