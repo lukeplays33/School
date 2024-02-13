b50a178a64662eae856e73ba1c66cdefc867b7d0
= <lukehoogenboom@gmail.com>
2024-02-13 12:00:19 +0100

    typo

--- pages/responsive/js/main.js
+++ pages/responsive/js/main.js
@@ -2,9 +2,8 @@ let nav = document.getElementsByTagName('nav')[0];
 
 document.getElementById('cheeseBurger').onclick = function () {
     if(nav.style.dispaly == 'flex') {
-        alert('test')
-        nav.style.dispaly = 'none';
+        nav.style.display = 'none';
     } else {
-        nav.style.dispaly = 'flex';
+        nav.style.display = 'flex';
     }
 }
