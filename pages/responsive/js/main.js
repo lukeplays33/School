869cfe26d8ee3e192c22e20a47f555f528d55d87
= <lukehoogenboom@gmail.com>
2024-02-13 12:08:06 +0100

    ugh

--- pages/responsive/js/main.js
+++ pages/responsive/js/main.js
@@ -1,6 +1,7 @@
 let nav = document.getElementsByTagName('nav')[0];
 
 document.getElementById('cheeseBurger').onclick = function () {
+    alert(nav.style.display)
     if(nav.style.display == 'flex') {
         nav.style.display = 'none';
     } else {
