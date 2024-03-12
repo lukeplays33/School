f62096ecee27e1dfb4fea9df25b73c74b50ce3f4
= <lukehoogenboom@gmail.com>
2024-03-12 10:58:45 +0100

    added menu switch to html

--- pages/css menu/js/main.js
+++
@@ -1,9 +0,0 @@
-let nav = document.getElementById('nav');
-
-document.getElementById('cheeseBurger').onclick = function () {
-    if(nav.style.display == 'flex') {
-        nav.style.display = 'none';
-    } else {
-        nav.style.display = 'flex';
-    }
-}
