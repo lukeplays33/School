17d045de5e972c36b942588efaa149fb22695694
luke hoogenboom <6035070@mborijnland.nl>
2024-11-26 10:11:55 +0100

    test

--- pages/php/database.php
+++ pages/php/database.php
@@ -15,6 +15,18 @@ $username = "root"; // Change this to your MySQL username
 $password = ""; // Change this to your MySQL password 
 // Connect to MySQL database using PDO 
 $pdo = new PDO("mysql:host=$host; dbname=$dbname", $username, $password);
+
+// Prepare SQL query to fetch all data from the "users" table 
+$query= "SELECT FROM gebruikers";
+$statement = $pdo->prepare($query); // Execute the query Sstatement->execute(); // Fetch all rows as an associative and nummeric array 
+$users = $statement->fetchAll(); // Check if there are any users 
+if (count($users) > 0) { // Output the data 
+    foreach ($users as $user) {
+         echo "User ID: $user[0]." . 'Username:' . $user['username'] . ", password:" . $user['password']."<br>";
+    }
+    } else { // Add other fields as needed 
+        echo "No users found in the database.";
+    }
 ?>
 </body>
 </html>
